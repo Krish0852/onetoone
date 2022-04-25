efb7ee16f65c50dc4fb2e62227959b4d70fe6439
M1082731 <M1082731@MT-HHJXYY2>
2022-04-25 13:15:09 +0530

    commit

---
+++ onetoone/src/main/java/com/example/onetoone/onetooneApplication.java
@@ -0,0 +1,12 @@
+package com.example.onetoone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.onetoone.onetooneApplication;
+
+@SpringBootApplication
+public class onetooneApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(onetooneApplication.class, args);
+	}
+}
